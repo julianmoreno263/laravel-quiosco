2ec2bfca6091db42fac12c1e02540d1210f45357
Julián Moreno <julianmoreno263@gmail.com>
2024-03-29 18:04:02 -0500

    Creando LoginRequest para crear las reglas de validacion del login y los mensajes de error

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\RegistroRequest;
 
 class AuthController extends Controller
@@ -19,7 +21,7 @@ class AuthController extends Controller
 
             'name'=>$data['name'],
             'email'=>$data['email'],
-            'password'=>bcrypt($data['name']),
+            'password'=>bcrypt($data['password'])
 
         ]);
 
@@ -31,9 +33,28 @@ class AuthController extends Controller
         ];
     }
 
-    public function login(Request $request){
+    public function login(LoginRequest $request){
         
+        $data=$request->validated();
+
+        //revisar el password
+        if (!Auth::attempt($data)) {
+              
+            return response([
+                'errors'=>['El email o el password son incorrectos']
+            ],422);
+        }
+
+        //autenticar al usuario
+        $user=Auth::user();
+
+        return [
+
+            'token'=>$user->createToken('token')->plainTextToken,
+            'user'=>$user
+        ];
     }
+
 
     public function logout(Request $request){
 
